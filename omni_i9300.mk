dafa7abdd338cd83677e8a9cfa081666ddd072d3
ttucker23 <ad.hoc91@gmail.com>
2013-12-29 20:13:12 -0700

    Update omni_i9300.mk

--- omni_i9300.mk
+++ omni_i9300.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, device/samsung/i9300/device.mk)
+$(call inherit-product, device/samsung/T999/device.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := omni_t999
